50d55966e61ffe458a5044ccc47212f24774accf
Marais Rossouw <me@maraisr.com>
2016-05-25 16:48:07 +1000

    chore: testing added

---
+++ test/entry.spec.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+var PocketSmith = require('../dist')
+
+describe('PocketSmith', function () {
+
+    it('works', function () {
+      expect(true).to.equal(true);
+    });
+
+});
