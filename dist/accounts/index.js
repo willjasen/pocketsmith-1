325e7853a2454b9c81e0aee4707678a074706284
Marais Rossouw <me@maraisr.com>
2016-06-13 13:09:25 +1000

    refactor: we dont need the `new` because the modules are all static'ish

--- dist/accounts/index.js
+++ dist/accounts/index.js
@@ -3,17 +3,17 @@ var client_1 = require('../client');
 var Accounts = (function () {
     function Accounts() {
     }
-    Accounts.prototype.get = function (id, callback) {
+    Accounts.get = function (id, callback) {
         return client_1.default.get("accounts/" + id, callback);
     };
-    Accounts.prototype.getAllByUser = function (userId, callback) {
+    Accounts.getAllByUser = function (userId, callback) {
         return client_1.default.get("users/" + userId + "/accounts", callback);
     };
-    Accounts.prototype.getAllByInstitution = function (institutionId, callback) {
+    Accounts.getAllByInstitution = function (institutionId, callback) {
         return client_1.default.get("institutions/" + institutionId + "/accounts", callback);
     };
     return Accounts;
 }());
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = Accounts;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvYWNjb3VudHMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQVdBLHVCQUFtQixXQUFXLENBQUMsQ0FBQTtBQUUvQjtJQUFBO0lBWUEsQ0FBQztJQVhBLHNCQUFHLEdBQUgsVUFBSSxFQUFVLEVBQUUsUUFBbUI7UUFDbEMsTUFBTSxDQUFDLGdCQUFNLENBQUMsR0FBRyxDQUFDLGNBQVksRUFBSSxFQUFFLFFBQVEsQ0FBQyxDQUFDO0lBQy9DLENBQUM7SUFFRCwrQkFBWSxHQUFaLFVBQWEsTUFBYyxFQUFFLFFBQW1CO1FBQy9DLE1BQU0sQ0FBQyxnQkFBTSxDQUFDLEdBQUcsQ0FBQyxXQUFTLE1BQU0sY0FBVyxFQUFFLFFBQVEsQ0FBQyxDQUFDO0lBQ3pELENBQUM7SUFFRCxzQ0FBbUIsR0FBbkIsVUFBb0IsYUFBcUIsRUFBRSxRQUFtQjtRQUM3RCxNQUFNLENBQUMsZ0JBQU0sQ0FBQyxHQUFHLENBQUMsa0JBQWdCLGFBQWEsY0FBVyxFQUFFLFFBQVEsQ0FBQyxDQUFDO0lBQ3ZFLENBQUM7SUFDRixlQUFDO0FBQUQsQ0FBQyxBQVpELElBWUM7QUFFRDtrQkFBZSxRQUFRLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvYWNjb3VudHMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQVdBLHVCQUFtQixXQUFXLENBQUMsQ0FBQTtBQUUvQjtJQUFBO0lBWUEsQ0FBQztJQVhPLFlBQUcsR0FBVixVQUFXLEVBQVUsRUFBRSxRQUFtQjtRQUN6QyxNQUFNLENBQUMsZ0JBQU0sQ0FBQyxHQUFHLENBQUMsY0FBWSxFQUFJLEVBQUUsUUFBUSxDQUFDLENBQUM7SUFDL0MsQ0FBQztJQUVNLHFCQUFZLEdBQW5CLFVBQW9CLE1BQWMsRUFBRSxRQUFtQjtRQUN0RCxNQUFNLENBQUMsZ0JBQU0sQ0FBQyxHQUFHLENBQUMsV0FBUyxNQUFNLGNBQVcsRUFBRSxRQUFRLENBQUMsQ0FBQztJQUN6RCxDQUFDO0lBRU0sNEJBQW1CLEdBQTFCLFVBQTJCLGFBQXFCLEVBQUUsUUFBbUI7UUFDcEUsTUFBTSxDQUFDLGdCQUFNLENBQUMsR0FBRyxDQUFDLGtCQUFnQixhQUFhLGNBQVcsRUFBRSxRQUFRLENBQUMsQ0FBQztJQUN2RSxDQUFDO0lBQ0YsZUFBQztBQUFELENBQUMsQUFaRCxJQVlDO0FBWkQ7MEJBWUMsQ0FBQSJ9
---
+++ dist/categories/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var Categories = (function () {
+    function Categories() {
+    }
+    return Categories;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Categories;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY2F0ZWdvcmllcy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7SUFBQTtJQUVBLENBQUM7SUFBRCxpQkFBQztBQUFELENBQUMsQUFGRCxJQUVDO0FBRkQ7NEJBRUMsQ0FBQSJ9
