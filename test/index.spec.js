1bd2e4b23baec7224e51b40417b195409fa9e846
Marais Rossouw <me@maraisr.com>
2016-06-28 22:03:28 +1000

    test: fixing the coverage bug

--- test/index.spec.js
+++ test/index.spec.js
@@ -17,34 +17,34 @@ var methods = [
 	'init'
 ];
 
-describe('PocketSmith', function() {
-	before(function() {
+describe('PocketSmith', function () {
+	before(function () {
 		this.smith = (new PocketSmith('TOKEN'));
 	});
 
-	describe('instance', function() {
+	describe('instance', function () {
 		methods.forEach((method) => {
-			it(`should have a ${method} method`, function() {
+			it(`should have a ${method} method`, function () {
 				expect(this.smith[method]).to.exist;
 			})
 		});
 	});
 
-	describe('@init', function() {
-		beforeEach(function() {
+	describe('@init', function () {
+		beforeEach(function () {
 			var req = nock(API)
 				.get('/me')
 				.reply(200);
 		});
-		
-		afterEach(function() {
+
+		afterEach(function () {
 			nock.cleanAll();
 		});
 
-		it('should expose the same methods', function(done) {
+		it('should expose the same methods', function (done) {
 			var self = this;
-			
-			this.smith.init().then(function(smith) {
+
+			this.smith.init().then(function (smith) {
 				methods.forEach((method) => {
 					expect(smith[method]).to.exist;
 				});
@@ -54,9 +54,9 @@ describe('PocketSmith', function() {
 			});
 		})
 
-		it('should return a promise', function(done) {		
+		it('should return a promise', function (done) {
 			var self = this;
-					
+
 			this.smith.init()
 				.then(() => {
 					self.smith.Me = void 0;
@@ -67,12 +67,16 @@ describe('PocketSmith', function() {
 					done();
 				})
 		});
-		
+
 		it('should return the cached copy of me if its called again', function (done) {
+			var self = this;
+
 			// Codecoverage will illustrate this
-			this.smith.init();
-			this.smith.init();
-			
+			this.smith.init().then(function () {
+				self.smith.init();
+			});
+
+
 			this.smith.Me = void 0;
 			done();
 		});
