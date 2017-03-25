c6a17138b1302f82ee6f66257230e92994a471f3
Marais Rossouw <me@maraisr.com>
2017-03-25 16:26:58 +1000

    chore: updated deps

--- dist/exceptions/index.js
+++ dist/exceptions/index.js
@@ -1,6 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function NotInMeContext() {
     return new Error('PocketSmith: Please init a `me` PocketSmith inistance. Eg: (new PocketSmith(\'token\')).init().then(() => { ... })');
 }
 exports.NotInMeContext = NotInMeContext;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvZXhjZXB0aW9ucy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7SUFDQyxNQUFNLENBQUMsSUFBSSxLQUFLLENBQUMsb0hBQW9ILENBQUMsQ0FBQztBQUN4SSxDQUFDO0FBRmUsc0JBQWMsaUJBRTdCLENBQUEifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvZXhjZXB0aW9ucy9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBO0lBQ0MsTUFBTSxDQUFDLElBQUksS0FBSyxDQUFDLG9IQUFvSCxDQUFDLENBQUM7QUFDeEksQ0FBQztBQUZELHdDQUVDIn0=
